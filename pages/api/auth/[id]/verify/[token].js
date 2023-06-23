add9dc5e020aeb25f1ebf20bc39e68750ae5c8d7
Himanshu Goyal <82256199+himanshuGoyal52@users.noreply.github.com>
2023-06-23 20:36:06 +0530

    db connect

--- pages/api/auth/[id]/verify/[token].js
+++ pages/api/auth/[id]/verify/[token].js
@@ -1,12 +1,14 @@
 import User from "../../../../../models/User";
+import db from "../../../../../utils/db";
 import Token from "../../../../../models/Token";
 
 const handler = async (req , res) => {
+       try{
+        db.connect();
         const user = await User.findById(req.body.id);
-        
         const token = await Token.findOne({
-                userId : req.body.id,
-                token : req.body.token,
+            userId : req.body.id,
+            token : req.body.token,
         });
 
         if(!token){
@@ -16,7 +18,11 @@ const handler = async (req , res) => {
         user.verified = true;
         await user.save();
         await token.remove();
-        res.status(200).send({message : 'Email verified!!' , user : user});
+        db.disconnect();
+        res.status(200).send({message : 'Email verified!!', user : user});
+    }catch(err){
+        res.status(404).send({message : 'some error occured'});
+    }
     
 };
 export default handler;
