c22a0650746c25deb2be4d07ab87a7a496a6e6fb
Himanshu Goyal <82256199+himanshuGoyal52@users.noreply.github.com>
2023-06-23 13:11:31 +0530

    Update [token].js

--- pages/api/auth/[id]/verify/[token].js
+++ pages/api/auth/[id]/verify/[token].js
@@ -2,11 +2,12 @@ import User from "../../../../../models/User";
 import Token from "../../../../../models/Token";
 
 const handler = async (req , res) => {
-
-        let user = await User.findById(req.body.id);
+    try{
+        const user = await User.findById(req.body.id);
         
-        let token = await Token.findOne({
-            token : req.body.token,
+        const token = await Token.findOne({
+                userId : req.body.id,
+                token : req.body.token,
         });
 
         if(!token){
@@ -17,6 +18,8 @@ const handler = async (req , res) => {
         await user.save();
         await token.remove();
         res.status(200).send({message : 'Email verified!!' , user : user});
-    
+    }catch(err){
+        res.status(404).send({message : 'some error occured'});
+    }
 };
 export default handler;
